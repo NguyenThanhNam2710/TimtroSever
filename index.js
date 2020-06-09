497f9b2f271bc885136b809592a240bb73e0766b
Namnt <nguyenthanhnam2k27@gmai.com>
2020-06-10 00:19:36 +0700

    commit: đăng nhập sever, màn hình chính(màn thống kê), đăng xuất

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let postSchema = require('./model/postSchema');
 let adminSchema = require('./model/adminSchema');
 
 let User = db.model('User', userSchema, 'users');
-let Admin = db.model('Admin', userSchema, 'admins');
+let Admin = db.model('Admin', adminSchema, 'admins');
 
 db.connect('mongodb+srv://Nhom5qlda14351:quanlyduan123@cluster0-z9led.mongodb.net/TimtroDatabase?retryWrites=true&w=majority', {}).then(function (res) {
     console.log('conected');
@@ -30,6 +30,165 @@ app.engine('.hbs', hbs({
 }))
 app.set('view engine', '.hbs')
 app.listen(9090);
+// phần sever
+
+// đăng nhập
+app.get('/', function (request, response) {
+    response.render('login', {status: 'none', user: '', pass: ''});
+});
+
+let nameDN = '', allAdmin = '';
+// kiểm tra đăng nhập nếu đúng hiện trang trủ index
+app.get('/index', async function (request, response) {
+
+    let listAdmin = await Admin.find({}).lean();   //dk
+
+    allAdmin = listAdmin.length;
+
+
+    let user = request.query.user;
+    let pass = request.query.pass;
+    let sm = request.query.sm;
+
+    if (sm == 1) {
+        nameDN = user;
+        console.log(user + " " + sm);
+    }
+
+    let admins = await Admin.find({username: user, password: pass}).lean();   //dk
+
+    if (admins.length <= 0 && sm == 1) {
+        response.render('login', {
+            status: 'block',
+            data: 'Không thể đăng nhập, kiểm tra lại tài khoản và mật khẩu của bạn.',
+            user: '',
+            pass: ''
+        });
+    } else {
+        response.render('index', {
+            status: 'none',
+            user: nameDN,
+            pass: pass,
+        });
+    }
+
+
+});
+// thêm sửa xóa tài khoản admin
+app.get('/createAdAc', async function (request, response) {
+    let a = await Admin.find({}).lean();   //dk
+    let search = request.query.search;
+    let nameSP = request.query.nameSP;
+    if (search == 1 && nameSP) {
+        let seachAdmin = await Admin.find({username: nameSP}).lean();
+        response.render('createAdAc', {
+            status: 'none',
+            data: seachAdmin,
+        });
+    } else {
+        let sm = request.query.sm;
+        let del = request.query.del;
+        let edit = request.query.ud;
+        if (sm == 1) {
+            let nUser = request.query.nUser;
+            let nPass = request.query.nPass;
+
+            let findAdmin = await Admin.find({username: nUser}).lean();   //dk
+
+
+            if (findAdmin.length <= 0) {
+                let newAdmin = new Admin({
+                    username: nUser,
+                    password: nPass,
+                });
+                let status = await newAdmin.save();
+                let admins = await Admin.find({}).lean();   //dk
+
+                if (status) {
+                    response.render('createAdAc', {
+                        status: 'block',
+                        textAlert: 'Tạo tài khoản thành công.',
+                        data: admins,
+                    });
+                } else {
+                    response.render('createAdAc', {
+                        status: 'block',
+                        textAlert: 'Tạo tài khoản thất bại.',
+                        data: admins,
+                    });
+                }
+            } else {
+
+                response.render('createAdAc', {
+                    status: 'block',
+                    textAlert: 'Tài khoản đã tồn tại.Mời tạo tài khoản khác !',
+                    data: a,
+                });
+            }
+        } else if (del == 1) {
+            console.log('del ad ' + request.query.idAD);
+            let status = await Admin.findByIdAndDelete(request.query.idAD);
+            let admins = await Admin.find({}).lean();   //dk
+            if (status) {
+                response.render('createAdAc', {
+                    status: 'block',
+                    textAlert: 'Xóa tài khoản thành công.',
+                    data: admins,
+                });
+            } else {
+                response.render('createAdAc', {
+                    status: 'block',
+                    textAlert: 'Xóa tài khoản thất bại.',
+                    data: admins,
+                });
+            }
+        } else if (edit == 1) {
+            let nId = request.query.nId;
+            let nUser = request.query.nUser;
+            let nPass = request.query.nPass;
+            console.log('edit ad ' + request.query.nId);
+
+            let admins = await Admin.find({username: nUser, password: nPass}).lean();   //dk
+            if (admins.length <= 0) {
+                console.log(nId + "edit ad");
+                let status = await Admin.findByIdAndUpdate(nId, {
+                    username: nUser,
+                    password: nPass
+                });
+                let nAdmins = await Admin.find({}).lean();
+                if (status) {
+                    response.render('createAdAc', {
+                        status: 'block',
+                        textAlert: 'Cập nhật tài khoản thành công.',
+                        data: nAdmins,
+                    });
+                } else {
+                    response.render('createAdAc', {
+                        status: 'block',
+                        textAlert: 'Cập nhật tài khoản thất bại.',
+                        data: nAdmins,
+                    });
+                }
+            } else {
+                let nAdmins = await Admin.find({}).lean();
+                response.render('createAdAc', {
+                    status: 'block',
+                    textAlert: 'Cập nhật tài khoản thất bại. Tài khoản đã tồn tại.',
+                    data: nAdmins,
+                });
+            }
+
+        } else {
+            del = 0;
+            edit = 0;
+            response.render('createAdAc', {
+                status: 'none',
+                data: a,
+            });
+        }
+    }
+});
+
 
 
 // phần kết nối sever với app
